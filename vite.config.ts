ae1841a68ccdf33961e39d0d6d869416be2157f4
reeves-48777 <durandthomas@protonmail.com>
2024-05-21 20:53:25 +0200

    feat - added chroma wasm as a dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import wasm from 'vite-plugin-wasm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), wasm()],
   resolve: {
     alias: {
-      '@': '/src/'
-    }
-  }
-})
+      '@': '/src/',
+      '@wasm': '/chroma-wasm/pkg',
+    },
+  },
+});
