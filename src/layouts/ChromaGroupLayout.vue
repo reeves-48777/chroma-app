526935fc15fd4f98e84ca92960c6e4dbf572582d
Thomas Durand <durandthomas@protonmail.com>
2024-05-21 23:03:42 +0200

    refacto - extracted group layout into its own component

---
+++ src/layouts/ChromaGroupLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="group" :class="groupMetaClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+enum Layout {
+  Horizontal = 'horizontal',
+  Vertical = 'vertical',
+}
+
+interface GroupProps {
+  layout?: string;
+}
+const props = withDefaults(defineProps<GroupProps>(), {
+  layout: Layout.Vertical,
+});
+
+const groupMetaClass = computed(() => {
+  return props.layout === Layout.Horizontal
+    ? 'flex-row space-x-2'
+    : 'flex-col space-y-2';
+});
+</script>
+
+<style scoped>
+.group {
+  @apply flex;
+}
+</style>
